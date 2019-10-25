87eec8b1b9d3da590f147efca9d42d8632717d87
Brandon In <brandonin@me.com>
2019-10-25 15:13:12 -0700

    Added functionality to count and name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+
+  clickCounter: number = 0;
+  name: string = '';  // add this
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  countClick() {
+    this.clickCounter += 1;
   }
 
 }
